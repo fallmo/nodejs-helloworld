8310349612d2566f647c38ff90effe107f627122
Mo <ps.hackmaster@gmail.com>
2022-07-07 10:00:54 +0000

    test hook and trigger

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send(`Hello World!\n Hello Message: ${process.env.HELLO_MESSAGE}`);
 });
 
 app.listen(8080, function () {
--- hook.js
+++ hook.js
@@ -1,1 +1,8 @@
+const exec = require('child_process').exec
 console.log("Hook executed")
+
+exec("curl -k -X POST https://api.cluster.heritage.africa:6443/apis/build.openshift.io/v1/namespaces/investigate/buildconfigs/pyth/webhooks/bb29bc38082e4961/generic", (...rest) => {
+	console.log(rest);
+});
+
+
